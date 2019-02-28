c62a15254a4361801347f3b94afb1c49130f716e
Jan Bielecki <jbielecki1@interactive0002.cis.gov.pl>
2019-02-28 14:05:42 +0100

    XGB estimators all results

--- Classification/NemaSource/stats.py
+++ Classification/NemaSource/stats.py
@@ -2,9 +2,10 @@
 from calc import *
 
 modelName = '10e7'
-directory = '/home/jasiek/Desktop/Studia/PracaMagisterska/Nema_Image_Quality/'
+# directory = '/home/jasiek/Desktop/Studia/PracaMagisterska/Nema_Image_Quality/'
+directory = '/mnt/opt/groups/jpet/NEMA_Image_Quality/3000s/'
 fileName = 'NEMA_IQ_384str_N0_1000_COINCIDENCES_part00'
-df, xTrain, xTest, yTrain, yTest = createLearningBatches(directory + fileName, 10000)
+df, xTrain, xTest, yTrain, yTest = createLearningBatches(directory + fileName, 10000000)
 pPs = df[df["class"] == 1]
 reconstruction2D(pPs, title = "IEC reconstruction", modelName = modelName)
 plotAngleVsTime(pPs, name = modelName, modelName = modelName)
