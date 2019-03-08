3e03ff1c88fb9b7c612bc7e55f3a9489e88280d4
K4liber <janbielecki94@gmail.com>
2019-03-08 10:04:38 +0100

    Create real reconstruciton and angle obly for test data

--- Classification/NemaSource/stats.py
+++ Classification/NemaSource/stats.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3.6
 from calc import *
 
-modelName = '10e7'
+modelName = '2e7'
+mkdir_p(modelName)
 # directory = '/home/jasiek/Desktop/Studia/PracaMagisterska/Nema_Image_Quality/'
 directory = '/mnt/opt/groups/jpet/NEMA_Image_Quality/3000s/'
 fileName = 'NEMA_IQ_384str_N0_1000_COINCIDENCES_part00'
 df, xTrain, xTest, yTrain, yTest = createLearningBatches(directory + fileName, 10000000)
-pPs = df[df["class"] == 1]
+xTest["class"] = yTest 
+pPs = xTest[xTest["class"] == 1]
 reconstruction2D(pPs, title = "IEC reconstruction", modelName = modelName)
 plotAngleVsTime(pPs, name = modelName, modelName = modelName)
