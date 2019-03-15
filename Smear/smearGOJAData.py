226096cafb5799827ea1886198a604b7af4ced24
K4liber <janbielecki94@gmail.com>
2019-03-15 20:18:51 +0100

    Read GOJA data from all files p5

--- Smear/smearGOJAData.py
+++ Smear/smearGOJAData.py
@@ -9,16 +9,23 @@ sys.path.append(projectDir + '/Classification/NemaSource')
 from calc import dataFrameNames
 import numpy as np
 import dask.dataframe as dd
+import pandas as pd
 
 def loadData(dataSize):
     directory = '/home/jasiek/Desktop/Studia/PracaMagisterska/Nema_Image_Quality/3000s/'
     if sys.argv[1] == "K": directory = '/mnt/opt/groups/jpet/NEMA_Image_Quality/3000s/'
     global df
-    df = dd.read_csv(
-        directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_part*', 
-        sep = "\t", names = dataFrameNames()
-    )
-    df = df.head(dataSize, npartitions=-1)
+    if dataSize > 10000000:
+        df = dd.read_csv(
+            directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_part*', 
+            sep = "\t", names = dataFrameNames()
+        )
+    else:
+        df = pd.read_csv(
+            directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_part00', 
+            sep = "\t", names = dataFrameNames()
+        )
+        df = df.head(dataSize)
 
 dataSize = int(sys.argv[2])
 loadData(dataSize)
