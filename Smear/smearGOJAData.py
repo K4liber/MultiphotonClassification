212d0c2901fe4a6e2dfcf17f28f9b0888daa52cd
K4liber <janbielecki94@gmail.com>
2019-03-15 19:36:35 +0100

    Read GOJA data from all files p3

--- Smear/smearGOJAData.py
+++ Smear/smearGOJAData.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3.6
 
 import sys
-# sys.path.append('/media/jasiek/F686ACAE86AC7133/Dokumenty/Studia/PracaMagisterska/MultiphotonClassification/Classification/NemaSource')
-sys.path.append('/mnt/home/jbielecki1/MultiphotonClassification/Classification/NemaSource')
+projectDir = '/media/jasiek/F686ACAE86AC7133/Dokumenty/Studia/PracaMagisterska/MultiphotonClassification/'
+if sys.argv[1] == "K":
+    projectDir = '/mnt/home/jbielecki1/MultiphotonClassification/'
+
+sys.path.append(projectDir + '/Classification/NemaSource')
 from calc import dataFrameNames
 import numpy as np
 import dask.dataframe as dd
 
 def loadData(dataSize):
-    directory = '/home/jasiek/Desktop/Studia/PracaMagisterska/Nema_Image_Quality/'
-    # directory = '/mnt/opt/groups/jpet/NEMA_Image_Quality/3000s/'
+    directory = '/home/jasiek/Desktop/Studia/PracaMagisterska/Nema_Image_Quality/3000s/'
+    if sys.argv[1] == "K": directory = '/mnt/opt/groups/jpet/NEMA_Image_Quality/3000s/'
     global df
     df = dd.read_csv(
         directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_part*', 
         sep = "\t", names = dataFrameNames()
     ).head(dataSize, npartitions=-1)
 
-dataSize = int(sys.argv[1])
+dataSize = int(sys.argv[2])
 loadData(dataSize)
 print(len(df["t1"]))
