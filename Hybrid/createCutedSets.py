3f4f0753e048cb769a36e41703000205bfeb9144
K4liber <janbielecki94@gmail.com>
2019-06-01 19:53:50 +0200

    Create cuted sets p5

--- Hybrid/createCutedSets.py
+++ Hybrid/createCutedSets.py
@@ -61,7 +61,7 @@ def createLearningBatches(filePath):
 
 directory = '/mnt/home/jbielecki1/NEMA/'
 fileName = 'cutData'
-mkdir_p((directory + "cut_parts4")
+mkdir_p(directory + "cut_parts4")
 xTrain, xTest, yTrain, yTest = createLearningBatches(directory + fileName)
 pickle.dump(xTest, open(directory + "cut_parts4" + "/xTest", 'wb'), protocol=4)
 pickle.dump(xTrain, open(directory + "cut_parts4" + "/xTrain", 'wb'), protocol=4)
