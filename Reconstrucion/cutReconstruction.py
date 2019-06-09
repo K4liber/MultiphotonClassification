99eb8b7d8f8f52e046d6d50c5c2853ff5261c4e8
K4liber <janbielecki94@gmail.com>
2019-06-09 10:31:53 +0200

    Cut reconstruction parts 16

--- Reconstrucion/cutReconstruction.py
+++ Reconstrucion/cutReconstruction.py
@@ -9,7 +9,21 @@ pathToFile = '/mnt/home/jbielecki1/NEMA/'
 data1 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part01', 'rb'))
 data2 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part02', 'rb'))
 data3 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part03', 'rb'))
-data = pd.concat([data1, data2, data3])
+data4 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part04', 'rb'))
+data5 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part05', 'rb'))
+data6 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part06', 'rb'))
+data7 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part07', 'rb'))
+data8 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part08', 'rb'))
+data10 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part10', 'rb'))
+data11 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part11', 'rb'))
+data12 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part12', 'rb'))
+data13 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part13', 'rb'))
+data14 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part14', 'rb'))
+data15 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part15', 'rb'))
+data16 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part16', 'rb'))
+data17 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part17', 'rb'))
+
+data = pd.concat([data1, data2, data3, data4, data5, data6, data7, data8, data10, data11, data12, data13, data14, data15, data16, data17], ignore_index = True)
 
 # Cut params
 width1 = 1050.0
@@ -65,4 +79,4 @@ cuttedData = data.apply(cutGeometry, axis = 1)
 pPsPredictedPositive = pd.DataFrame(pd.concat([cuttedData[cuttedData == 1], cuttedData[cuttedData == 2]]).sort_index())
 dataPositive = data.iloc[list(pPsPredictedPositive.index),:]
 dataRec = dataPositive.iloc[:,:16]
-dataRec.to_csv(pathToFile + 'cutReconstruction', sep = "\t", header = False, index = False)
+dataRec.to_csv(pathToFile + 'cutReconstruction_parts16', sep = "\t", header = False, index = False)
