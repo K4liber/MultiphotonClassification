cbd6e1717db7163f7fc6b19b424129b43714bc1a
K4liber <janbielecki94@gmail.com>
2019-06-10 11:01:03 +0200

    ADA reconstruction parts 16

--- Reconstrucion/adaReconstruction.py
+++ Reconstrucion/adaReconstruction.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python3.6
 
 import pickle
-from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
 
-directory = '/mnt/home/jbielecki1/NEMA/'
+pathToFile = '/mnt/home/jbielecki1/NEMA/'
 modelFileName = '10000000/ADA/adaEstimators1000Depth6'
 
-model = pickle.load(open(directory + modelFileName, 'rb'))
-data = pickle.load(open(directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part01', 'rb'))
-data2 = pickle.load(open(directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part02', 'rb'))
-data3 = pickle.load(open(directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part03', 'rb'))
-allData = pd.concat([data, data2, data3])
+model = pickle.load(open(pathToFile + modelFileName, 'rb'))
+data1 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part01', 'rb'))
+data2 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part02', 'rb'))
+data3 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part03', 'rb'))
+data4 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part04', 'rb'))
+data5 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part05', 'rb'))
+data6 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part06', 'rb'))
+data7 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part07', 'rb'))
+data8 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part08', 'rb'))
+data10 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part10', 'rb'))
+data11 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part11', 'rb'))
+data12 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part12', 'rb'))
+data13 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part13', 'rb'))
+data14 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part14', 'rb'))
+data15 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part15', 'rb'))
+data16 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part16', 'rb'))
+data17 = pickle.load(open(pathToFile + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part17', 'rb'))
+
+data = pd.concat([data1, data2, data3, data4, data5, data6, data7, data8, data10, data11, data12, data13, data14, data15, data16, data17], ignore_index = True)
 
 codes = {1:1, 2:0, 3:0, 4:0}
-y = allData["class"].map(codes)
-x = allData.drop(["t1", "t2", "sX1", "sY1", "sZ1", "class", "rError"], axis = 1)
+y = data["class"].map(codes)
+x = data.drop(["t1", "t2", "sX1", "sY1", "sZ1", "class", "rError"], axis = 1)
 
-test_accuracy = []
-max_acc = 0
-y_pred_prob = []
-y_pred = []
-
-for test_predicts_el in model.staged_predict_proba(x):
-    y_pred_prob_el = test_predicts_el[:,1]
-    y_pred_el = y_pred_prob_el > 0.5
-    acc = accuracy_score(y_pred_el, np.array(y))
-    
-    if acc > max_acc:
-        max_acc = acc
-        y_pred_prob = y_pred_prob_el
-        y_pred = y_pred_el
-        
-    test_accuracy.append(acc)
+y_pred_prob = model.predict_proba(x)
+y_pred = y_pred_prob > 0.5
 
 pPsPredictedPositive = x[y_pred]
 dataPositive = data.iloc[list(pPsPredictedPositive.index),:]
 dataRec = dataPositive.iloc[:,:16]
 
-dataRec.to_csv(directory + 'adaReconstruction', sep = "\t", header = False, index = False)
+dataRec.to_csv(directory + 'adaReconstruction_parts16', sep = "\t", header = False, index = False)
