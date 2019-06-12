2851b1f22ea35e3c5e0f9367dc0cc98d5fd4900f
K4liber <janbielecki94@gmail.com>
2019-06-12 13:24:56 +0200

    xgbReconstrucion with threshold as param

--- Reconstrucion/xgbReconstruction.py
+++ Reconstrucion/xgbReconstruction.py
@@ -3,30 +3,33 @@
 import pickle
 import numpy as np
 import pandas as pd
+import sys
 
-directory = '/mnt/home/jbielecki1/NEMA/'
+threshold = float(sys.argv[1])
+pathToFile = '/mnt/home/jbielecki1/NEMA/'
 modelFileName = '10000000/XGB/xgbEstimators1000Depth6'
-max_depth = 6
-feature_names = ['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'vol1', 'vol2', 'e1', 'e2', 'dt', 'rX1', 'rY1', 'rZ1', 'volD', 'lorL', 'deg3D', 'deg2D', 'rL', 'eSum']
+fileName = 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part'
+dataPositiveParts = []
+model = pickle.load(open(pathToFile + modelFileName, 'rb'))
 
-model = pickle.load(open(directory + modelFileName, 'rb'))
-data = pickle.load(open(directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part01', 'rb'))
-data2 = pickle.load(open(directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part02', 'rb'))
-data3 = pickle.load(open(directory + 'NEMA_IQ_384str_N0_1000_COINCIDENCES_PREPARED_part03', 'rb'))
-allData = pd.concat([data, data2, data3])
+def getGOJAFormatPositivePrediction(filePath):
+    print("Processing file '" + filePath + "'.")
+    data = pickle.load(open(filePath, 'rb'))
+    x = data.drop(["t1", "t2", "sX1", "sY1", "sZ1", "class", "rError"], axis = 1)
+    x.columns = ["f" + str(x) for x in range(20)]
+    y_pred_prob = model.predict_proba(x)
+    y_pred = y_pred_prob > threshold
+    pPsPredictedPositive = x[y_pred[:,1]]
+    dataPositive = data.iloc[list(pPsPredictedPositive.index),:]
+    return dataPositive.iloc[:,:16]
 
-codes = {1:1, 2:0, 3:0, 4:0}
-y = allData["class"].map(codes)
-x = allData.drop(["t1", "t2", "sX1", "sY1", "sZ1", "class", "rError"], axis = 1)
-x.columns = ["f" + str(x) for x in range(20)]
+for i in range(8):
+    dataPositiveParts.append(getGOJAFormatPositivePrediction(pathToFile + fileName + '0' + str(i+1)))
 
-y_pred_prob = model.predict_proba(x)
-x.columns = feature_names
-y_pred_prob = y_pred_prob
-y_pred = y_pred_prob > 0.5
+for i in range(9):
+    if i != 3:
+        dataPositiveParts.append(getGOJAFormatPositivePrediction(pathToFile + fileName + '1' + str(i)))
+    
 
-pPsPredictedPositive = x[y_pred[:,1]]
-dataPositive = data.iloc[list(pPsPredictedPositive.index),:]
-dataRec = dataPositive.iloc[:,:16]
-
-dataRec.to_csv(directory + 'xgbReconstruction_parts3', sep = "\t", header = False, index = False)
+dataRec = pd.concat(dataPositiveParts)
+dataRec.to_csv(pathToFile + 'xgbReconstruction_parts3_' + str(threshold), sep = "\t", header = False, index = False)
